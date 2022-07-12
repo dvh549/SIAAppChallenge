5dc7061f527dfc69afba7257b4e115c94503fa22
dereckSOC <dereckrupertt@gmail.com>
2022-07-12 22:28:42 +0800

    ejs support

--- frontend/routes/default.js
+++ frontend/routes/default.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 //index route
-router.get("/login", function(req, res) {
+router.get("/", function(req, res) {
     res.render("index.ejs");
 })
 
